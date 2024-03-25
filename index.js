a698b893df3e88188bad38ec960dc2d0ad3a393c
DanielJoel <danpersie53@gmail.com>
2024-03-25 23:50:41 +0300

    control lab

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let overLimitMessage = 'No can do.'
+let thirtyBucksMessage = 'I will gladly take your thirty bucks.'
+let twentyBucksMessage = 'That will be twenty bucks.'
+let freeSampleMessage = 'This one is on me!'
+let message
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+
+    message = freeSampleMessage
+  }
+  else if (feet > 400 && feet < 2000) {
+
+    message = twentyBucksMessage
+
+  }
+  else if (feet > 2000 && feet < 2500) {
+    message = thirtyBucksMessage
+
+  }
+  else if (feet > 2500) {
+    message = overLimitMessage
+  }
+
+  return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let nycMessage = 'Ok, sounds good.'
+let notNycMessage = 'No go.'
+let destination
+
+function ternaryCheckCity(destination) {
+  destination === "NYC" ? (message = nycMessage) : (message = notNycMessage);
+  return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (true) {
+    case tip === 'generous':
+      return "Thank you so much.";
+    case tip === 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
